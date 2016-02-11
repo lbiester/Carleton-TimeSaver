4f91f43552f0ba626fa38116d35be183c3247ac1
Laura Biester <laurajpb@gmail.com>
2016-02-11 11:17:42 -0600

    App working

--- fill_form.js
+++ fill_form.js
@@ -9,18 +9,22 @@ chrome.runtime.onMessage.addListener(
 );
 
 function saveTimes() {
-    console.log("saving times...");
     dictionary = {}; // find stuff
     for (var i = 1; i <= 28; i++) {
-        console.log(document.getElementById("LIST_VAR4_" + i));
-        console.log(document.getElementById("LIST_VAR5_" + i));
+        var start_time = "LIST_VAR4_" + i;
+        var end_time = "LIST_VAR5_" + i;
+        dictionary[start_time] = document.getElementById(start_time).value;
+        dictionary[end_time] = document.getElementById(end_time).value;
     }
     chrome.storage.sync.set(dictionary);
 }
 
 function loadTimes() {
-    console.log("loading times...");
     chrome.storage.sync.get(function(items) {
-        console.log(items);
+        for (var key in items) {
+            if (items.hasOwnProperty(key) && document.getElementById(key) !== null) {
+                document.getElementById(key).value = items[key];
+            }
+        }
     });
 }
